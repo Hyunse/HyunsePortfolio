cee286e2486bacccc444d9aed6df3e85d7805262
KimHyunse <anzmf12@naver.com>
2018-06-28 17:24:08 -0400

    Content 추가

--- src/App.vue
+++ src/App.vue
@@ -7,9 +7,8 @@
         <div class="split-content-vertically-center">
           <div class="split-intro">
             <div>
-              <!-- <img src="/static/images/me.jpg" alt="" style="width:30%; height:30%;"> -->
+              <img src="/static/images/me.jpg" alt="" style="width:30%; height:30%;">
             </div>
-            <!-- <h1>Kim Hyunse</h1> -->
             <span class="tagline">Web Developer</span>
           </div>
           <div class="split-bio">
@@ -27,18 +26,30 @@
               <ul>
                 <li v-for="( project, index ) in projects" :key="index">
                   <a href="#none" v-on:click="show(project)">{{ project.name }}</a>
-                  <p class="split-icon">
-                    <i class="far fa-clock"></i>
-                  </p>
-                  <p class="split-info">
-                    {{ project.time }}
-                  </p>
-                  <p class="split-icon">
-                    <i class="far fa-building"></i>
-                  </p>
-                  <p class="split-info">
-                    {{ project.company }}
-                  </p>
+                  <div>
+                    <p class="split-icon">
+                      <i class="far fa-clock"></i>
+                    </p>
+                    <p class="split-info">
+                      {{ project.time }}
+                    </p>
+                  </div>
+                  <div>
+                    <p class="split-icon">
+                      <i class="far fa-building"></i>
+                    </p>
+                    <p class="split-info">
+                      {{ project.company }}
+                    </p>
+                  </div>
+                  <div>
+                    <p class="split-icon">
+                      <i class="fas fa-child"></i>
+                    </p>
+                    <p class="split-info">
+                      {{ project.position }} Developer
+                    </p>
+                  </div>
                 </li>
               </ul>
             </div>
@@ -86,7 +97,10 @@
             </carousel>
           </div>
           <div class="modal-content">
+             <p style="font-weight:bold;">Info</p>
              <p class="project-info">{{modal.info}}</p>
+             <p style="font-weight:bold;">Technologies</p>
+             <p>{{modal.tech}}</p>
           </div>
         </div>
       </div>
@@ -111,76 +125,90 @@ export default {
           name: 'Remote AR Homepage',
           company: 'Virnect',
           time: '2017.06 ~ 2017.12',
-          info: `Remote AR Homepage has 3 part pages. First, main page is introducing RemoteAR application.
-                 And next part is that RemoteAR service page. Also, it has analytics pages to give usage information for 
-                 customers`,
+          position: 'Web Front-End',
+          info: `Remote AR Homepage has 3 part pages. First of all, main page is introducing RemoteAR application.
+                 And there is RemoateAR service page. Also, it has analytics pages to give usage information for 
+                 RemoteAR customers.`,
           images: [
             '/static/images/remotear/remotear_1.png',
             '/static/images/remotear/remotear_2.png',
             '/static/images/remotear/remotear_3.png',
-            '/static/images/remotear/remotear_4.png']
+            '/static/images/remotear/remotear_4.png'],
+          tech: 'Vue.js, Javascript, ES6, Responsive Web, C3 Chart, Node.js Express, Azure.'
         },
         {
           name: 'Software Center Busan Univ',
           company: 'Virnect',
           time: '2017.10 ~ 2017.11',
-          info: `This site is for university students and teachers for introducing Busan University's software center
-                and sharing resources, such as pictures and video`,
+          position: 'Web Front-End',
+          info: `It's a website for university students and teachers to introduce Busan University's software center
+                and share resources, such as pictures and video`,
           images: [
             '/static/images/busan/busan_1.jpg',
             '/static/images/busan/busan_2.jpg',
             '/static/images/busan/busan_3.jpg',
-            '/static/images/busan/busan_4.jpg']
+            '/static/images/busan/busan_4.jpg'],
+          tech: 'Jquery, Javascript, Node.js Express, GoogleMap API, Youtube API, Azure.'
         },
         {
           name: 'See Real Homepage',
           company: 'Virnect',
           time: '2017.04 ~ 2017.05',
+          position: 'Web Front-End',
           info: `It's a single page web to inroduce SeeReal application.`,
           images: [
-            '/static/images/seereal/seereal_1.png']
+            '/static/images/seereal/seereal_1.png'],
+          tech: 'Spring Framework, Java, Mybatis, Jquery, Responsive Web, Ms-sql, Azure.'
         },
         {
           name: 'Processed Meat,Red Meat Calculation Program APP',
           company: 'ABC Solutuon',
           time: '2016.12 ~ 2016.12',
+          position: 'Full Stack',
           info: `It's a hybrid web app to calculate calorie in a processed meat and red meat user eat. 
           I made a registration page in which user can register a food they eat,
           a board page in which user can check calorie in each time period,
-          a graph page to use JqPlot Chart, etc.`,
+          a graph page to use JqPlot Chart.`,
           images: [
             '/static/images/cal/cal_1.jpg',
             '/static/images/cal/cal_2.jpg',
             '/static/images/cal/cal_3.jpg',
-            '/static/images/cal/cal_4.jpg']
+            '/static/images/cal/cal_4.jpg'],
+          tech: 'eGovFramework, Java, Mybatis, Jquery, JqPlot Chart, Altibase.'
         },
         {
           name: 'Kyongnam Bank Internet Banking',
           company: 'ABC Solutuon',
           time: '2016.02 ~ 2016.12',
+          position: 'Full Stack',
           info: `It's internet banking system. I made foreign exchange part like Foreign Currency Remittance, 
-          Direct Remittance Service, Foreign Currency Time Deposit, Exchange rate inquiry, etc.`,
+          Direct Remittance Service, Foreign Currency Time Deposit, Exchange rate inquiry and so on.`,
           images: [
             '/static/images/knbank/k_1.jpg',
             '/static/images/knbank/k_2.jpg',
             '/static/images/knbank/k_3.jpg',
-            '/static/images/knbank/k_4.jpg']
+            '/static/images/knbank/k_4.jpg'],
+          tech: 'UBiz30 Framework, IB20 Framework, Java, Jquery, Oracle.'
         },
         {
           name: 'Nutrient Calculation Program WEB',
           company: 'ABC Solutuon',
           time: '2015.12 ~ 2016.01',
-          info: `Nutritional contents program is a tool calculating nutritional contents in food. I did data migration, made a nutritional contents program, made its admin page to use WebSquare.`,
+          position: 'Full Stack',
+          info: `Nutritional contents program is a tool calculating nutritional contents in food.
+          I did data migration, made a nutritional contents program, made its admin pages to use WebSquare.`,
           images: [
             '/static/images/eat/eat_1.jpg',
             '/static/images/eat/eat_2.jpg',
             '/static/images/eat/eat_3.jpg',
-            '/static/images/eat/eat_4.jpg']
+            '/static/images/eat/eat_4.jpg'],
+          tech: 'eGovFramework, Java, Mybatis, Jquery, WebSquare5, Altibase.'
         },
         {
           name: 'Bosch Master Tool',
           company: 'ABC Solutuon',
           time: '2015.06 ~ 2015.12',
+          position: 'Full Stack',
           info: `Master Tool is a web application for a field worker and officer in Bosch.
                 It was my first web project in my career. I made a analytics page using c3.js,
                 batch program to send Email on specific day and time, 
@@ -188,7 +216,8 @@ export default {
           images: [
             '/static/images/bosch/mastertool_1.png',
             '/static/images/bosch/mastertool_2.png',
-            '/static/images/bosch/mastertool_3.png']
+            '/static/images/bosch/mastertool_3.png'],
+          tech: 'Spring Data JPA, Hibernate, Jquery, DHtmlX Grid, C3 Chart, Ms-sql, Bootstrap.'
         }
       ]
     }
@@ -198,6 +227,7 @@ export default {
       this.modal.title = obj.name
       this.modal.info = obj.info
       this.modal.images = obj.images
+      this.modal.tech = obj.tech
       this.$modal.show('project-modal')
     }
   },
